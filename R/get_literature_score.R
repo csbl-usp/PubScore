f77ccdc820b47aec7c9bd2f262c9deda1ff9cbb2
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2019-05-15 13:16:05 -0300

    Work on the PubScore DB

--- R/get_literature_score.R
+++ R/get_literature_score.R
@@ -59,21 +59,21 @@
 #' @return A dataframe with the literature scores.
 #' @export
 #' @examples
-#'  gene <- 'CD4'
-#'  terms_of_interest <-
-#'    c(
-#'      "CD4 T cell",
-#'      "CD14+ Monocyte",
-#'      "B cell",
-#'      "CD8 T cell",
-#'      "FCGR3A+ Monocyte",
-#'      "NK cell",
-#'      "Dendritic cell",
-#'      "Megakaryocyte",
-#'      'immunity'
-#'    )
-#'  get_literature_score(gene, terms_of_interest, 500)
-#'  get_literature_score(gene, terms_of_interest, max_score = Inf)
+#'   gene <- 'CD4'
+#'   terms_of_interest <-
+#'     c(
+#'       "CD4 T cell",
+#'       "CD14+ Monocyte",
+#'       "B cell",
+#'       "CD8 T cell",
+#'       "FCGR3A+ Monocyte",
+#'       "NK cell",
+#'       "Dendritic cell",
+#'       "Megakaryocyte",
+#'       'immunity'
+#'     )
+#'   get_literature_score(gene, terms_of_interest, max_score = 500)
+#'   get_literature_score(gene, terms_of_interest, max_score = Inf)
 
 
 
