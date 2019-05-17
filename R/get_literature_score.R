4159f9176e45a6c130c15cf2d7a36b856bcb0004
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2019-05-17 19:08:02 -0300

    Bug fix and code organization

--- R/get_literature_score.R
+++ R/get_literature_score.R
@@ -59,22 +59,22 @@
 #' @return A dataframe with the literature scores.
 #' @export
 #' @examples
-   gene <- 'CD4'
-   terms_of_interest <-
-     c(
-       "CD4 T cell",
-       "CD14+ Monocyte",
-       "B cell",
-       "CD8 T cell",
-       "FCGR3A+ Monocyte",
-       "NK cell",
-       "Dendritic cell",
-       "Megakaryocyte",
-       'immunity'
-     )
-   get_literature_score(gene, terms_of_interest, max_score = 500)
-   get_literature_score(gene, terms_of_interest, max_score = Inf)
-
+#'    gene <- 'CD4'
+#'    terms_of_interest <-
+#'      c(
+#'        "CD4 T cell",
+#'        "CD14+ Monocyte",
+#'        "B cell",
+#'        "CD8 T cell",
+#'        "FCGR3A+ Monocyte",
+#'        "NK cell",
+#'        "Dendritic cell",
+#'        "Megakaryocyte",
+#'        'immunity'
+#'      )
+#'    get_literature_score(gene, terms_of_interest, max_score = 500)
+#'    get_literature_score(gene, terms_of_interest, max_score = Inf)
+#' 
 
 
 get_literature_score <-
