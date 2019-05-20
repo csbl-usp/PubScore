e265c9d633537dff1a08f0708a812e908d9e997f
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2019-05-20 15:16:27 -0300

    Preparing blacklist for ambiguous gene names such as MARCH

--- R/test_literature_score.R
+++ R/test_literature_score.R
@@ -55,10 +55,13 @@ test_literature_score <-
       pb_test_score$tick()
 
       tryCatch({
+        print(i)
+        print(terms_of_interest)
         ps_object <- PubScore::get_literature_score(i, terms_of_interest, 
                                                     max_score =  literature_object$max_score,
                                                     show_progress = FALSE)
         new_line <- data.frame(i, ps_object$counts)
+        print(ps_object$gene_literature_score)
         simulation_of_literature_null <- rbind(simulation_of_literature_null, new_line)
         Sys.sleep(0.2)}, error = function(e){print(e)
         })
@@ -69,7 +72,7 @@ test_literature_score <-
       distribution_of_scores <- c()
       for (i in seq_len(100000)){    
         genes_to_sample_now <- sample(total_genes, genes_to_sample)
-        simu_now <- simulation_of_literature_null[simulation_of_literature_null$Var1 %in% genes_to_sample_now,]$count
+        simu_now <- simulation_of_literature_null[simulation_of_literature_null$Gene %in% genes_to_sample_now,]$count
         list_score <- sum(simu_now) / genes_to_sample
         distribution_of_scores <- c(distribution_of_scores,list_score )
         
