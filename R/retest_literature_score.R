a43e8bf8e990e1b465b4911fee17353c4e0a4999
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2019-05-24 17:01:11 -0300

    Mostly working on removal of ambiguously named genes'

---
+++ R/retest_literature_score.R
@@ -0,0 +1,180 @@
+
+
+.getSimulation_retest <- function(literature_object, ambiguous = c()) {
+  simulation_of_literature_null <-
+    literature_object$all_gene_combinations[!literature_object$all_gene_combinations$Gene %in% ambiguous, ]
+  n_genes <- length(levels(literature_object$counts$Gene))
+  total_genes <- simulation_of_literature_null$Gene
+  message('Running 100000 simulations')
+  distribution_of_scores <- c()
+  for (i in seq_len(100000)) {
+    genes_to_sample_now <- sample(total_genes, n_genes)
+    simu_now <-
+      simulation_of_literature_null[simulation_of_literature_null$Gene %in% genes_to_sample_now, ]$count
+    list_score <- sum(simu_now) / n_genes
+    distribution_of_scores <- c(distribution_of_scores, list_score)
+    
+  }
+  
+  distribution_of_scores <- data.frame(d = distribution_of_scores)
+  return(distribution_of_scores)
+  
+}
+
+#  Re-Estimates p-value for a literature score provided an object 
+# from the get_literature_score function
+# lubianat 24/05/2019
+
+#' retest_literature_score
+#'
+#' Regets a pvalue, by simulation, for a literature score.
+#'
+#' @param literature_object The object returned by the test_literature_score function
+#' @param new_ambigous_term_list A vector containing a list of genes deemed ambiguous by the user. 
+#' If left blank, a default list is used.
+#' @param remove_ambiguous If TRUE, ambiguously named genes (such as "MARCH") will be removed. 
+#' Defaults to TRUE.  
+#' @import rentrez
+#' @import progress
+#' @return A dataframe with the literature scores.
+#' @export
+#' @examples
+#' 
+#' genes <- c('CD14')
+#' terms_of_interest <-
+#'   c(
+#'     "CD4 T cell",
+#'     "CD14+ Monocyte",
+#'     "B cell",
+#'     "CD8 T cell",
+#'     "FCGR3A+ Monocyte",
+#'     "NK cell",
+#'     "Dendritic cell",
+#'     "Megakaryocyte",
+#'     'immunity'
+#'   )
+#' literature_object <- PubScore::get_literature_score(genes, terms_of_interest)
+#' total_genes <- c('CD14','notagenemock1','notagenemock2','notagenemock3', "PROC", "IMPACT")
+#' literature_object <- test_literature_score(literature_object, total_genes, remove_ambiguous = F)
+#' literature_object <- retest_literature_score(literature_object, remove_ambiguous = T)
+#' 
+retest_literature_score <-
+  function(literature_object,
+           new_ambigous_term_list = FALSE,
+           remove_ambiguous = FALSE) {
+    
+    if (any(new_ambigous_term_list != FALSE)) {
+      distribution_of_scores <-
+        .getSimulation_retest(literature_object, new_ambigous_term_list)
+      score <-
+        mean(literature_object$counts$count[!literature_object$counts$Gene %in% new_ambigous_term_list])
+      pvalue <-
+        sum(distribution_of_scores[, 1] >= score) / length(distribution_of_scores[, 1])
+      literature_object[['p_value']] <- pvalue
+      print('The p-value by simulation removing the new list of ambiguous genes is:')
+      print(pvalue) 
+      
+      return(literature_object)
+    }
+    
+    if (remove_ambiguous == FALSE) {
+      distribution_of_scores <- .getSimulation_retest(literature_object)
+      
+      score <- mean(literature_object$counts$count)
+      pvalue <-
+        sum(distribution_of_scores[, 1] >= score) / length(distribution_of_scores[, 1])
+      
+      print('The p-value by simulation  is:')
+      print(pvalue) 
+      
+      literature_object[['p_value']] <- pvalue
+      
+      return(literature_object)
+      
+      
+      
+    }
+    
+    
+    
+    if (remove_ambiguous == TRUE) {
+      ambiguous_terms <-
+        c(
+          "PC",
+          "JUN",
+          "MARCH",
+          "IMPACT",
+          "ACHE",
+          "SRI",
+          "SET",
+          "CS",
+          "PROC",
+          "MET",
+          "SHE",
+          "CAD",
+          "DDT",
+          "PIGS",
+          "SARS",
+          "REST",
+          "GC",
+          "CP",
+          "STAR",
+          "SI",
+          "GAN",
+          "MARS",
+          "SDS",
+          "AGA",
+          "NHS",
+          "CPE",
+          "POR",
+          "MAX",
+          "CAT",
+          "LUM",
+          "ANG",
+          "POLE",
+          "CLOCK",
+          "TANK",
+          "ITCH",
+          "SDS",
+          "AES",
+          "CIC",
+          "FST",
+          "CAPS",
+          "COPE",
+          "F2",
+          "AFM",
+          "SPR",
+          "PALM",
+          "C2",
+          "BAD",
+          "GPI",
+          "CA2",
+          "SMS",
+          "INVS",
+          "WARS",
+          "HP",
+          "GAL",
+          "SON",
+          "AFM",
+          "BORA",
+          "MBP",
+          "MAK",
+          "MALL",
+          "COIL",
+          "CAST "
+        )
+      distribution_of_scores2 <-
+        .getSimulation_retest(literature_object, ambiguous_terms)
+      
+      score <-
+        mean(literature_object$counts$count[!literature_object$counts$Gene %in% ambiguous_terms])
+      pvalue <-
+        sum(distribution_of_scores2[, 1] >= score) / length(distribution_of_scores2[, 1])
+      literature_object[['p_value']] <- pvalue
+      
+      print('The p-value by simulation removing the default list of ambiguous genes is:')
+      print(pvalue) 
+      
+      return(literature_object)
+    }
+  }
--- R/test_literature_score.R
+++ R/test_literature_score.R
@@ -1,45 +1,67 @@
+.getSimulation_test <- function(literature_object, ambiguous = c()) {
+  simulation_of_literature_null <-
+    literature_object$all_gene_combinations[!literature_object$all_gene_combinations$Gene %in% ambiguous, ]
+  
+  n_genes <- length(levels(literature_object$counts$Gene[!literature_object$counts$Gene %in% ambiguous ]))
+  total_genes <- levels(droplevels(simulation_of_literature_null$Gene))
+  message('Running 100000 simulations')
+  
+  distribution_of_scores <- c()
+  for (i in seq_len(100000)) {
+    genes_to_sample_now <- sample(total_genes, n_genes)
+    simu_now <-
+      simulation_of_literature_null[simulation_of_literature_null$Gene %in% genes_to_sample_now, ]$count
+    list_score <- sum(simu_now) / n_genes
+    distribution_of_scores <- c(distribution_of_scores, list_score)
+    
+  }
+  
+  distribution_of_scores <- data.frame(d = distribution_of_scores)
+  return(distribution_of_scores)
+  
+}
 
 #  Estimates p-value for a literature score provided an object 
 #from the get_literature_score function
 # lubianat 28/09/2018
 
-#' get_literature_score
+#' test_literature_score
 #'
-#' Calculates the importance score for a given gene.
-#' The importance score is the total counts of articles in the pubmed
-#' database that are a result for that gene AND each term of a list
+#' gets a pvalue, by simulation, for a literature score.
 #'
 #' @param literature_object The object returned by the get_literature_score function
 #' @param total_genes A list of all the possible genes in your study. 
 #' Usually all the names in the rows of an "exprs" object.
 #' @param show_progress If TRUE, a progress bar is displayed. Defaults to True.
 #' @param verbose If TRUE, will display the index of the search occuring. Defaults to false.
+#' @param remove_ambiguous If TRUE, ambiguously named genes (such as "MARCH") will be removed. Defaults to TRUE.  
 #' @import rentrez
 #' @import progress
 #' @return A dataframe with the literature scores.
 #' @export
 #' @examples
-#'    genes <- c('CD4','CD8')
-#'    terms_of_interest <-
-#'      c(
-#'        "CD4 T cell",
-#'        "CD14+ Monocyte",
-#'        "B cell",
-#'        "CD8 T cell",
-#'        "FCGR3A+ Monocyte",
-#'        "NK cell",
-#'        "Dendritic cell",
-#'        "Megakaryocyte",
-#'        'immunity'
-#'      )
-#'  literature_object <- PubScore::get_literature_score(gene, terms_of_interest, 500)
-#'  total_genes <- c('CD4', 'CD8','CD7','ASDASD','ADSFASD','ADSFASDS', 'asdas')
-#'  literature_object <- test_literature_score(literature_object, total_genes)
-
+#'      genes <- c('CD14')
+#'      terms_of_interest <-
+#'        c(
+#'          "CD4 T cell",
+#'          "CD14+ Monocyte",
+#'          "B cell",
+#'          "CD8 T cell",
+#'          "FCGR3A+ Monocyte",
+#'          "NK cell",
+#'          "Dendritic cell",
+#'          "Megakaryocyte",
+#'          'immunity'
+#'        )
+#'    literature_object <- PubScore::get_literature_score(genes, terms_of_interest)
+#'    total_genes <- c('CD14', 'notagenemock1','notagenemock2','notagenemock3', "PROC", "IMPACT")
+#'    literature_object <- test_literature_score(literature_object, total_genes)
+#'  
 test_literature_score <-
   function(literature_object,
            total_genes,
            show_progress = TRUE,
+           remove_ambiguous = TRUE,
            verbose = FALSE) {
     terms_of_interest <- levels(literature_object$counts$Topic)
     genes_to_sample <- length(levels(literature_object$counts$Gene))
@@ -55,53 +77,53 @@ test_literature_score <-
       pb_test_score$tick()
 
       tryCatch({
-        print(i)
-        print(terms_of_interest)
         ps_object <- PubScore::get_literature_score(i, terms_of_interest, 
                                                     max_score =  literature_object$max_score,
                                                     show_progress = FALSE)
         new_line <- data.frame(i, ps_object$counts)
-        print(ps_object$gene_literature_score)
         simulation_of_literature_null <- rbind(simulation_of_literature_null, new_line)
         Sys.sleep(0.2)}, error = function(e){print(e)
         })
     }
       simulation_of_literature_null <- simulation_of_literature_null[simulation_of_literature_null$i != "",]  
       
-      message('Running 100000 simulations')
-      distribution_of_scores <- c()
-      for (i in seq_len(100000)){    
-        genes_to_sample_now <- sample(total_genes, genes_to_sample)
-        simu_now <- simulation_of_literature_null[simulation_of_literature_null$Gene %in% genes_to_sample_now,]$count
-        list_score <- sum(simu_now) / genes_to_sample
-        distribution_of_scores <- c(distribution_of_scores,list_score )
-        
-      }
-      
-      distribution_of_scores<- data.frame(d = distribution_of_scores)
-      
-      
-      library(ggplot2)
-      ggplot(distribution_of_scores, aes(x = d))+
-        geom_density() +
-        ggtitle('Density of the distribution of null scores(10^6 resamplings)') +
-        xlab('Average number of counts on PubMed') +
-        ylab('Proportion of gene-lists for each category')
-      
-      
-      score <- literature_object$list_literature_score
+      obj <- literature_object
+      obj[['all_gene_combinations']] = simulation_of_literature_null
       
 
+    if (remove_ambiguous == FALSE){
+      
+      distribution_of_scores <- .getSimulation_test(obj)
+      score <- mean(literature_object$counts$count)
       pvalue <-sum(distribution_of_scores[,1] >= score)/length(distribution_of_scores[,1])
-    
-    
-    print('The p-value by simulation is:')
-    print(pvalue) 
-    obj <- literature_object
-    obj[['all_gene_combinations']] = simulation_of_literature_null
-    obj[['p_value']] = pvalue
-    tested_score <- obj
-    return(tested_score)
+      
+      print('The p-value by simulation is:')
+      print(pvalue) 
+      
+      obj[['p_value']] = pvalue
+      return(obj)
+    }
 
+
+    if (remove_ambiguous == TRUE){
+    ambiguous_terms <- c("PC", "JUN", "IMPACT", "ACHE", "SRI", "SET", "CS", "PROC", 
+                         "MET", "SHE", "CAD", "DDT", "PIGS", "SARS", "REST", "GC", "CP", 
+                         "STAR", "SI", "GAN", "MARS", "SDS", "AGA", "NHS", "CPE", "POR", 
+                         "MAX", "CAT", "LUM", "ANG", "POLE", "CLOCK", "TANK", "ITCH", 
+                         "SDS", "AES", "CIC", "FST", "CAPS", "COPE", "F2", "AFM", "SPR", 
+                         "PALM", "C2", "BAD", "GPI", "CA2", "SMS", "INVS", "WARS", "HP", 
+                         "GAL", "SON", "AFM", "BORA", "MBP", "MAK", "MALL", "COIL", "CAST ")
     
+    distribution_of_scores <- .getSimulation_test(obj, ambiguous =  ambiguous_terms)
+    
+    score <- mean(literature_object$counts$count[!literature_object$counts$Gene %in% ambiguous_terms])
+    pvalue <-sum(distribution_of_scores[,1] >= score)/length(distribution_of_scores[,1])
+    
+    print('The p-value by simulation removing ambiguous gene names is:')
+    print(pvalue) 
+    
+    obj[['p_value']] = pvalue
+    return(obj)
+    }
   }
+    
